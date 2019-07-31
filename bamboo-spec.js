b2a2033b048388f686c2d26169110c54ba110efc
cambiph <incubusattax@gmail.com>
2019-07-31 14:27:07 +0200

    Voeg proxy toe als build arg zodat NPM packages de env variabel http_proxy en https_proxy kunnen overnemen

--- bamboo-spec.js
+++ bamboo-spec.js
@@ -1,15 +1,28 @@
-const yaml = require('js-yaml')
-const fs = require('fs');
+const yaml      = require('js-yaml')
+const fs        = require('fs');
+const package   = require('../../package.json');
+const options   = { lineWidth: 200 }
 
-const options = { lineWidth: 200 }
+let json;
+
+function readPackageJson() {
+    json = JSON.parse(package);
+}
+
+const key       = "PCAM"
+const planName  =  json.name
+const planKey   = "TEST"
+
+console.log(json.name)
+
 const content = {
     "project":
     {
-        "key": "PCAM",
+        "key": key,
         "plan":
         {
-            "name": "bamboo-spec-test",
-            "key": "TEST"
+            "name": planName,
+            "key": planKey
         }
     },
     "stages": [{
@@ -19,15 +32,16 @@ const content = {
                 "cp ${bamboo.gitconfig_path} ${bamboo.build.working.directory}",
                 "cp ${bamboo.npmrc_path} ${bamboo.build.working.directory}",
                 "cp ${bamboo.gitcredentials_path} ${bamboo.build.working.directory}",
-                "docker build --build-arg VERSION=patch --build-arg REPO=${bamboo.planRepository.repositoryUrl} --no-cache .",
+                "docker build --build-arg VERSION=patch --build-arg REPO=${bamboo.planRepository.repositoryUrl} --build-arg http_proxy=${bamboo.http_proxy} --build-arg https_proxy=${bamboo.http_proxy} --no-cache .",
                 "/opt/scripts/docker/stop-docker-containers.sh"
             ], "requirements": ["REMOTE_ONLY"] 
         }]
     }]
 }
 
-const fileContent = yaml.safeDump(content, options);
-fs.writeFile('./bamboo-specs/bamboo.yml', fileContent, (err) => {
+const yamlFile = yaml.safeDump(content, options);
+
+fs.writeFile('../../bamboo-specs/bamboo.yml', yamlFile, 'utf8', (err) => {
     if (err) throw err;
     console.log('File has been saved!');
 })
