b33a7872c1a8e5bb6f539d5d8bd51dbfcb127bcb
cambiph <incubusattax@gmail.com>
2019-09-19 11:55:35 +0200

    Refactor

--- bamboo-spec.js
+++ bamboo-spec.js
@@ -1,35 +1,23 @@
 const yaml = require('yaml');
-const fs = require('fs');
+const utils = require('./utils/util');
 
-const planProjectKey = 'PCAM';
-const planKey = 'ABC';
-const planName = 'Test';
+const planProjectKey = 'WEBCOM';
+const packageName = utils.getPackageName();
+const planKey = utils.getPlanKey();
+const planName = 'webcomponent-' + packageName;
 
-const jobKey = 'XYZ';
-
-const permissionPlanKey = planKey;
-
-function parseTemplate(fileName) {
-    const file = fs.readFileSync(fileName, 'utf8');
-    return yaml.parseDocument(file);
-}
-
-const spec = parseTemplate('templates/spec-template.yml').contents;
-const permissions = parseTemplate('templates/permissions-template.yml').contents;
-
+const spec = utils.getSpec();
+const permissions = utils.getPermissions();
 const plan = spec.get('plan');
+const job = spec.get('Build docker');
 
 plan.set('project-key', planProjectKey);
 plan.set('key', planKey);
 plan.set('name', planName);
 
-const job = spec.get('Build docker')
-job.set('key', jobKey);
+job.set('key', planProjectKey);
 
-permissions.get('plan').set('key', permissionPlanKey);
+permissions.get('plan').set('key', planKey);
 
-const specs = yaml.stringify(spec);
-fs.writeFileSync('bamboo-specs/bamboo.yml', specs);
-
-const permissions0 = yaml.stringify(permissions);
-fs.writeFileSync('bamboo-specs/permissions.yml', permissions0)
+utils.writeYaml(yaml.stringify(spec), '../../bamboo-specs/bamboo.yml');
+utils.writeYaml(yaml.stringify(permissions), '../../bamboo-specs/permissions.yml');
